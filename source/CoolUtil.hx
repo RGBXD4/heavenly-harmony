a1320cf0fd65d502de448445414a17690feda59e
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-30 06:24:24 +0800

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -9,9 +9,8 @@ import flixel.system.FlxSound;
 #if sys
 import sys.io.File;
 import sys.FileSystem;
-#else
-import openfl.utils.Assets;
 #end
+import openfl.utils.Assets;
 
 using StringTools;
 
@@ -61,11 +60,9 @@ class CoolUtil
 	public static function coolTextFile(path:String):Array<String>
 	{
 		var daList:Array<String> = [];
-		#if sys
-		if(FileSystem.exists(path)) daList = File.getContent(path).trim().split('\n');
-		#else
+		
 		if(Assets.exists(path)) daList = Assets.getText(path).trim().split('\n');
-		#end
+		
 
 		for (i in 0...daList.length)
 		{
