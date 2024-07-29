aa3bf021d1e96dd86eee1c022dfa5c18d33bcc15
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-30 06:56:31 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -10,7 +10,7 @@ import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.display.StageScaleMode;
-
+import haxe.io.Path;
 //crash handler stuff
 #if CRASH_HANDLER
 import lime.app.Application;
